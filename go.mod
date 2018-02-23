afbb0109878ef63b24c28d1ef849b335234b2761
Sarah Connor <sconnor@pivotal.io>
2018-02-23 20:12:37 +0000

    vgo hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/terminatingcode/hello"
+
+require "rsc.io/quote" v1.5.2
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main // import "github.com/terminatingcode/hello"
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+}
